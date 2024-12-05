3faf7ff9fcbe8cf5e0c7336932001252affa6c90
Abdul Kader <abdulkader0126@gmail.com>
2024-12-05 11:07:25 +0700

    layout admin asset

--- routes/kader.php
+++ routes/kader.php
@@ -6,9 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/about', function () {
     return view('pages.LandingPage.about.index');
 });
-Route::get('/classroom', function () {
-    return view('pages.LandingPage.classroom.index');
-});
-Route::get('/admin', function () {
-    return view('pages.admin.dashboard.index');
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('pages.admin.dashboard.index');
+    });
+
+    Route::get('/classroom', function () {
+        return view('pages.admin.classroom.index');
+    });
 });
