157d2385be12310a0ac31ae37dff3f39bbdf3b35
Abdul Kader <abdulkader0126@gmail.com>
2024-12-06 14:47:43 +0700

    update slicing choose role

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -67,95 +67,109 @@
                             <div class="">
                                 <div class="row justify-content-center">
                                     <div class="col-md-4 col-lg-4 col-sm-4">
-                                        <a href="/register/student">
-                                            <label>
-                                                <input type="hidden" name="product" class="card-input-element" />
-                                                <div class="card panel panel-default card-input">
-                                                    <div class="mt-2 px-3 mb-2">
-                                                        <div class="">
-                                                            <div class="d-flex gap-2">
+                                        <div class="card bg-info text-white">
+                                            <div class="card-body">
+                                                <a href="/register/student">
+                                                    <label>
+                                                        <input type="hidden" name="product" class="card-input-element" />
+                                                        <div class="panel panel-default card-input">
+                                                            <div class="mt-2 text-center mb-2">
                                                                 <div class="">
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="32"
-                                                                        height="32" viewBox="0 0 24 24">
-                                                                        <g fill="none" stroke="currentColor"
-                                                                            stroke-dasharray="20" stroke-dashoffset="20"
-                                                                            stroke-linecap="round" stroke-linejoin="round"
-                                                                            stroke-width="2">
-                                                                            <path
-                                                                                d="M6 19v-1c0 -2.21 1.79 -4 4 -4h4c2.21 0 4 1.79 4 4v1">
-                                                                                <animate fill="freeze"
-                                                                                    attributeName="stroke-dashoffset"
-                                                                                    dur="0.2s" values="20;0" />
-                                                                            </path>
-                                                                            <path
-                                                                                d="M12 11c-1.66 0 -3 -1.34 -3 -3c0 -1.66 1.34 -3 3 -3c1.66 0 3 1.34 3 3c0 1.66 -1.34 3 -3 3Z">
-                                                                                <animate fill="freeze"
-                                                                                    attributeName="stroke-dashoffset"
-                                                                                    begin="0.2s" dur="0.2s"
-                                                                                    values="20;0" />
-                                                                            </path>
-                                                                        </g>
-                                                                    </svg>
+                                                                    <div class="d-flex gap-2">
+                                                                        <div class="">
+                                                                            <svg xmlns="http://www.w3.org/2000/svg" width="35"
+                                                                                height="35" viewBox="0 0 24 24">
+                                                                                <g fill="none" stroke="white"
+                                                                                    stroke-dasharray="20" stroke-dashoffset="20"
+                                                                                    stroke-linecap="round" stroke-linejoin="round"
+                                                                                    stroke-width="2">
+                                                                                    <path
+                                                                                        d="M6 19v-1c0 -2.21 1.79 -4 4 -4h4c2.21 0 4 1.79 4 4v1">
+                                                                                        <animate fill="freeze"
+                                                                                            attributeName="stroke-dashoffset"
+                                                                                            dur="0.2s" values="20;0" />
+                                                                                    </path>
+                                                                                    <path
+                                                                                        d="M12 11c-1.66 0 -3 -1.34 -3 -3c0 -1.66 1.34 -3 3 -3c1.66 0 3 1.34 3 3c0 1.66 -1.34 3 -3 3Z">
+                                                                                        <animate fill="freeze"
+                                                                                            attributeName="stroke-dashoffset"
+                                                                                            begin="0.2s" dur="0.2s"
+                                                                                            values="20;0" />
+                                                                                    </path>
+                                                                                </g>
+                                                                            </svg>
+                                                                        </div>
+                                                                        <div class="panel-heading text-white">
+                                                                            <h5 class="mt-1 text-white">
+                                                                                Student
+                                                                            </h5>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="panel-body">
+                                                                    </div>
                                                                 </div>
-                                                                <div class="panel-heading">Student</div>
-                                                            </div>
-                                                            <div class="panel-body">
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                </div>
-                                            </label>
-                                        </a>
+                                                    </label>
+                                                </a>
+                                            </div>
+                                        </div>
                                     </div>
                                     <div class="col-md-4 col-lg-4 col-sm-4">
-                                        <a href="/register/teacher">
-                                            <label>
-                                                <input type="hidden" name="product" class="card-input-element" />
-                                                <div class="card panel panel-default card-input">
-                                                    <div class="mt-2 px-3 mb-2">
-                                                        <div class="">
-                                                            <div class="d-flex gap-2">
+                                        <div class="card bg-secondary">
+                                            <div class="card-body">
+                                                <a href="/register/teacher">
+                                                    <label>
+                                                        <input type="hidden" name="product" class="card-input-element" />
+                                                        <div class=" panel panel-default card-input">
+                                                            <div class="mt-2 mb-2">
                                                                 <div class="">
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="32"
-                                                                        height="32" viewBox="0 0 24 24">
-                                                                        <g fill="none" stroke="currentColor"
-                                                                            stroke-dasharray="20" stroke-dashoffset="20"
-                                                                            stroke-linecap="round" stroke-linejoin="round"
-                                                                            stroke-width="2">
-                                                                            <path
-                                                                                d="M6 19v-1c0 -2.21 1.79 -4 4 -4h4c2.21 0 4 1.79 4 4v1">
-                                                                                <animate fill="freeze"
-                                                                                    attributeName="stroke-dashoffset"
-                                                                                    dur="0.2s" values="20;0" />
-                                                                            </path>
-                                                                            <path
-                                                                                d="M12 11c-1.66 0 -3 -1.34 -3 -3c0 -1.66 1.34 -3 3 -3c1.66 0 3 1.34 3 3c0 1.66 -1.34 3 -3 3Z">
-                                                                                <animate fill="freeze"
-                                                                                    attributeName="stroke-dashoffset"
-                                                                                    begin="0.2s" dur="0.2s"
-                                                                                    values="20;0" />
-                                                                            </path>
-                                                                        </g>
-                                                                    </svg>
+                                                                    <div class="d-flex gap-2">
+                                                                        <div class="">
+                                                                            <svg xmlns="http://www.w3.org/2000/svg" width="32"
+                                                                                height="32" viewBox="0 0 24 24">
+                                                                                <g fill="none" stroke="white"
+                                                                                    stroke-dasharray="20" stroke-dashoffset="20"
+                                                                                    stroke-linecap="round" stroke-linejoin="round"
+                                                                                    stroke-width="2">
+                                                                                    <path
+                                                                                        d="M6 19v-1c0 -2.21 1.79 -4 4 -4h4c2.21 0 4 1.79 4 4v1">
+                                                                                        <animate fill="freeze"
+                                                                                            attributeName="stroke-dashoffset"
+                                                                                            dur="0.2s" values="20;0" />
+                                                                                    </path>
+                                                                                    <path
+                                                                                        d="M12 11c-1.66 0 -3 -1.34 -3 -3c0 -1.66 1.34 -3 3 -3c1.66 0 3 1.34 3 3c0 1.66 -1.34 3 -3 3Z">
+                                                                                        <animate fill="freeze"
+                                                                                            attributeName="stroke-dashoffset"
+                                                                                            begin="0.2s" dur="0.2s"
+                                                                                            values="20;0" />
+                                                                                    </path>
+                                                                                </g>
+                                                                            </svg>
+                                                                        </div>
+                                                                        <div class="panel-heading">
+                                                                            <h5 class="mt-1 text-white">
+                                                                                Teacher
+                                                                            </h5>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="panel-body">
+                                                                    </div>
                                                                 </div>
-                                                                <div class="panel-heading">Teacher</div>
-                                                            </div>
-                                                            <div class="panel-body">
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                </div>
-                                            </label>
-                                        </a>
+                                                    </label>
+                                                </a>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-
-                                <button type="submit" class="btn btn-two arrow-btn">Sign In<img
-                                        src="assets/img/icons/right_arrow.svg" alt="img"
-                                        class="injectable"></button>
                         </form>
+                        <div class="account__divider">
+                        </div>
                         <div class="account__switch">
-                            <p>Don't have an account?<a href="{{ route('register') }}">Sign Up</a></p>
+                            <p>Sudah mempunyai akun?<a href="{{ route('login') }}">Login</a></p>
                         </div>
                     </div>
                 </div>
