5b880e6dac164322f40070d284138245b8bac5b8
vanzy505 <ivantjmaulana@gmail.com>
2024-12-08 09:17:17 +0700

    upppppp

--- resources/views/pages/student/detailMateri.blade.php
+++ resources/views/pages/student/detailMateri.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.student.app')
 @section('content')
     <div class="container">
+        <h1>Isi materi di sini</h1>
         <a href="{{ route('course') }}" class="btn btn-info">Kembali</a>
     </div>
 @endsection
